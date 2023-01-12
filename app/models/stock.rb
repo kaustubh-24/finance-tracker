9d42bc8daa2c15a6ba90cb60d18d1ecaca2ba908
kaustubh-24 <kaustubh.pathak@coupa.com>
2023-01-12 11:54:47 +0530

    add stocks api and my_portfolio page search functionality

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,4 +2,12 @@ class Stock < ApplicationRecord
     def self.new_lookup(ticker_symbol)
         rand(300)
     end
+
+    def self.demo_lookup(ticker_symbol)
+        require 'open-uri'
+        require 'json'
+        info = JSON.parse(URI.open("https://data.nasdaq.com/api/v3/datasets/WIKI/#{ticker_symbol}.json").read)
+        new(ticker: ticker_symbol, name: info["dataset"]["name"][0...-52], last_price: info["dataset"]["data"][0][1])
+    end
+
 end
