56d379ea1e35567cec29f2ce5160ebf05b785ca3
kaustubh-24 <kaustubh.pathak@coupa.com>
2023-01-12 12:13:28 +0530

    add symbol validity check

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -6,8 +6,12 @@ class Stock < ApplicationRecord
     def self.demo_lookup(ticker_symbol)
         require 'open-uri'
         require 'json'
-        info = JSON.parse(URI.open("https://data.nasdaq.com/api/v3/datasets/WIKI/#{ticker_symbol}.json").read)
-        new(ticker: ticker_symbol, name: info["dataset"]["name"][0...-52], last_price: info["dataset"]["data"][0][1])
+        begin
+            info = JSON.parse(URI.open("https://data.nasdaq.com/api/v3/datasets/WIKI/#{ticker_symbol}.json").read)
+            return new(ticker: ticker_symbol, name: info["dataset"]["name"][0...-52], last_price: info["dataset"]["data"][0][1]) 
+        rescue => exception
+            return nil
+        end
     end
 
 end
