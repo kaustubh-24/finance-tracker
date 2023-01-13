8798af1204cc960b68af64637e031d529c430dd6
kaustubh-24 <kaustubh.pathak@coupa.com>
2023-01-13 12:27:44 +0530

    add many-to-many association between users and stocks

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,9 @@
 class Stock < ApplicationRecord
+    has_many :user_stocks
+    has_many :users, through: :user_stocks
+
+    validates :name, :ticker, presence: true
+
     def self.new_lookup(ticker_symbol)
         rand(300)
     end
